fda712c0731a9dc115ff3df9f51f360eee3bec7b
molovo <hi@molovo.co.uk>
2015-05-14 22:02:11 +0100

    More Testing. A few JS tweaks. New Jekyll version

--- _test/functional/open-case-study.js
+++ _test/functional/open-case-study.js
@@ -1,12 +1,12 @@
 module.exports = {
   'Open a Case Study': function ( browser ) {
     browser
-      .url( 'https://molovo.co' )
+      .url( 'http://molovo.dev' )
       .click( '#work-link' )
-      .waitForElementVisible( '.work li.haresfoot', 1000 )
+      .waitForElementVisible( '.work li.haresfoot', 3000 )
       .click( '.work li.haresfoot a' )
       .pause( 1000 )
-      .assert.urlEquals( 'https://molovo.co/work/haresfoot' )
+      .assert.urlEquals( 'http://molovo.dev/work/haresfoot' )
       .assert.title( 'Brewing Up a Storm - molovo. Web Design, Portsmouth' )
       .end();
   }
