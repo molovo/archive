1f47164eae57485ba57e247e1dbd6255864832e7
James Dinsdale <hi@molovo.co>
2017-10-26 12:18:42 +0100

    Update page titles on scroll

--- _assets/js/title.js
+++ _assets/js/title.js
@@ -4,29 +4,48 @@ export default class Title {
   /**
    * The page title as it is when the page loads
    *
-   * @type {String}
+   * @type {string}
    */
   original = window.pageTitle
 
   /**
+   * The page title currently in use
+   *
+   * @type {string}
+   */
+  current = this.original
+
+  /**
    * A replacement title to use when the page is hidden
    *
-   * @type {String}
+   * @type {string}
    */
   replacement = 'I Miss You! ❤'
 
   /**
+   * A list of elements which update the title
+   *
+   * @type {NodeList}
+   */
+  titleElements = document.querySelectorAll('[data-title]')
+
+  /**
+   * The current title in the header
+   */
+  headerTitle = document.querySelector('.header__title')
+
+  /**
    * A map of events to their respective titles
    *
-   * @type {Object}
+   * @type {object}
    */
   evtMap = {
-    focus: this.original,
-    focusin: this.original,
-    pageshow: this.original,
-    blur: this.replacement,
-    focusout: this.replacement,
-    pagehide: this.replacement
+    focus: 'current',
+    focusin: 'current',
+    pageshow: 'current',
+    blur: 'replacement',
+    focusout: 'replacement',
+    pagehide: 'replacement'
   }
 
   /**
@@ -34,7 +53,37 @@ export default class Title {
    */
   constructor () {
     // Register the visibility change listener
+    window.addEventListener('scroll', this.findCorrectTitle)
     document.addEventListener('visibilitychange', this.updatePageTitle)
+  }
+
+  /**
+   * Find the correct title based on the current scroll position
+   *
+   * @param {ScrollEvent} e
+   */
+  @bind
+  findCorrectTitle (e) {
+    let set = false
+
+    // Loop through each of the elements with titles
+    for (var i = 0; i < this.titleElements.length; i++) {
+      const el = this.titleElements[i]
+      if (this.isInViewport(el)) {
+        const title = el.dataset.title
+        this.current = title
+        document.title = title
+        this.headerTitle.innerHTML = title
+        set = true
+        break
+      }
+    }
+
+    if (!set) {
+      this.current = this.original
+      document.title = this.original
+      this.headerTitle.innerHTML = ''
+    }
   }
 
   /**
@@ -48,14 +97,32 @@ export default class Title {
 
     // If the event type exists in the map, set the right title
     if (evt.type in this.evtMap) {
-      document.title = this.evtMap[evt.type]
+      document.title = this[this.evtMap[evt.type]]
     } else {
       // Last ditch attempt
       if (document.hidden) {
         document.title = this.replacement
       } else {
-        document.title = this.original
+        document.title = this.current
       }
     }
+  }
+
+  /**
+   * Check if an element is currently within the viewport
+   *
+   * @return {bool}
+   */
+  isInViewport (el) {
+    if (window.pageYOffset < 300) {
+      return false
+    }
+
+    const {top, left, bottom, right} = el.getBoundingClientRect()
+
+    return top <= (window.innerHeight / 2) &&
+            bottom >= (window.innerHeight / 2) &&
+            left <= (window.innerWidth / 2) &&
+            right >= (window.innerWidth / 2)
   }
 }
