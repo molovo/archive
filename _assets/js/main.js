087d5f7f583c486f4c407188900fcaa87279188c
James Dinsdale <hi@molovo.co>
2017-10-26 12:33:31 +0100

    Temporarily disable turbolinks

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -10,25 +10,25 @@ import Images from './images'
 import Github from './github'
 import Animator from './animator'
 import ScrollSync from './scroll-sync'
-import Turbolinks from 'turbolinks'
+// import Turbolinks from 'turbolinks'
 
-document.addEventListener('turbolinks:click', () => {
-  document.documentElement.classList.add('loading')
-})
+// document.addEventListener('turbolinks:click', () => {
+//   document.documentElement.classList.add('loading')
+// })
 
-document.addEventListener('turbolinks:load', () => {
-  window.github = new Github()
-  window.projects = new Projects()
-  window.search = new Search()
-  window.menu = new Menu()
-  window.title = new Title()
-  window.likes = new Likes()
-  window.images = new Images()
-  window.scrollSync = new ScrollSync()
-  window.animator = new Animator()
+// document.addEventListener('turbolinks:load', () => {
+//   window.github = new Github()
+//   window.projects = new Projects()
+//   window.search = new Search()
+//   window.menu = new Menu()
+//   window.title = new Title()
+//   window.likes = new Likes()
+//   window.images = new Images()
+//   window.scrollSync = new ScrollSync()
+//   window.animator = new Animator()
 
-  document.documentElement.classList.remove('loading')
-})
+//   document.documentElement.classList.remove('loading')
+// })
 
 window.github = new Github()
 window.projects = new Projects()
@@ -40,6 +40,6 @@ window.images = new Images()
 window.scrollSync = new ScrollSync()
 window.animator = new Animator()
 
-document.documentElement.classList.remove('loading')
+// document.documentElement.classList.remove('loading')
 
-Turbolinks.start()
+// Turbolinks.start()
